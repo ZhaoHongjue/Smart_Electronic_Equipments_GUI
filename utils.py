1807eab334a8431ad6cb70179280cd110bea4e59
Zhao Hongjue <zhaohongjue123@163.com>
2021-10-19 18:41:43 +0800

    更新了Data的store

--- utils.py
+++ utils.py
@@ -7,28 +7,40 @@ class Chart:
     def __init__(self):
         self.fig, self.ax = plt.subplots(1, 1)
     
-    def plot(self, episode_rewards):
+    def plot(self, data, pattern):
         self.ax.clear()
-        self.ax.plot(episode_rewards)
-        self.ax.set_xlabel('iteration')
-        self.ax.set_ylabel('episode reward')
+        self.ax.plot(data)
+        
+        # 设置坐标
+        self.ax.set_xlabel('Time')
+        if pattern == 1:
+            self.ax.set_ylabel('Temperature')
+        elif pattern == 2:
+            self.ax.set_ylabel('Humidity')
+        elif pattern == 3:
+            self.ax.set_ylabel('Luminousintensity')
         self.fig.canvas.draw()
 
 # 存储数据
 class Data:
-    def __init__(self):
-        self.capacity = 10000
+    def __init__(self, capacity = 10):
+        self.capacity = capacity
         self.i = 0
         self.count = 0
         self.df = pd.DataFrame(index=range(self.capacity),
-                               columns=['节点1温度','节点1湿度','节点1光照',
-                                        '节点2温度','节点2湿度','节点2光照',
-                                        '节点3温度','节点3湿度','节点3光照'])
+                               columns=['1_T','1_H','1_L',
+                                        '2_T','2_H','2_L',
+                                        '3_T','3_H','3_L',
+                                        '4_T','4_H','4_L',])
     def store(self, *args):
-        self.memory.loc[self.i] = args
+        self.df.loc[self.i] = args
+        self.i = self.i + 1
 
     def clear(self):
         pass
 
     def save(self):
         self.df.to_csv('test.csv')
+
+    def load(self):
+        pass
