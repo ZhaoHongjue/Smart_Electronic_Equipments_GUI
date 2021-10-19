7d277977a6c71c7040fdc2a9d7200a9a089763c4
Zhao Hongjue <zhaohongjue123@163.com>
2021-10-19 19:35:18 +0800

    数据存储部分结束

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+import time
 
 # 画图
 class Chart:
@@ -20,27 +21,34 @@ class Chart:
         elif pattern == 3:
             self.ax.set_ylabel('Luminousintensity')
         self.fig.canvas.draw()
+        plt.savefig('curve.png',bbox_inches='tight')
 
 # 存储数据
 class Data:
-    def __init__(self, capacity = 10):
-        self.capacity = capacity
+    def __init__(self):
         self.i = 0
-        self.count = 0
-        self.df = pd.DataFrame(index=range(self.capacity),
-                               columns=['1_T','1_H','1_L',
+        self.df = pd.DataFrame(columns=['time', '1_T','1_H','1_L',
                                         '2_T','2_H','2_L',
                                         '3_T','3_H','3_L',
                                         '4_T','4_H','4_L',])
+
     def store(self, *args):
         self.df.loc[self.i] = args
         self.i = self.i + 1
 
-    def clear(self):
-        pass
+    def clear(self, save = True):
+        if save:
+            self.save()
+        self.df = pd.DataFrame(columns=['time','1_T','1_H','1_L',
+                                        '2_T','2_H','2_L',
+                                        '3_T','3_H','3_L',
+                                        '4_T','4_H','4_L',])
+        self.i = 0
 
-    def save(self):
-        self.df.to_csv('test.csv')
+    def save(self, name = None):
+        if name is None:
+            name = time.strftime("%Y%m%d%H%M%S", time.localtime())
+        self.df.to_csv(name + '.csv')
 
-    def load(self):
-        pass
+    def load(self, name = 'test'):
+        self.df = pd.read_csv(name + '.csv', index_col = 0)
