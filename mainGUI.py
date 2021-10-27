1b0ecc14f853113b49c6babbc88784d628dc5ef0
Zhao Hongjue <zhaohongjue123@163.com>
2021-10-27 11:18:29 +0800

    了解了线程的实现方式

--- mainGUI.py
+++ mainGUI.py
@@ -7,41 +7,29 @@ from utils import Data
 from CurveGUI import CurveGUI
 import numpy as np
 
+VarList = np.zeros(12)
 
-# def init():
-#     global self.VarList
-#     data = get_data()
-#     for i in range(len(data)):
-#         tempVar = IntVar()
-#         tempVar.set(data[i])
-#         self.VarList.append(tempVar)
-#     print(self.VarList)
+def get_data():
+    data = np.ones(12) * 22
+    data = data + np.random.randint(low = 0, high = 10, size = (12,)) * 3
+    return data
 
-# def get_data():
-#     data = np.ones(12) * 22
-#     data = data + np.random.randint(low = 0, high = 10, size = (12,)) * 3
-#     return data.tolist()
-
-# def update_data():
-#     global timer, self.VarList
-#     data = get_data()
-#     for i in range(len(data)):
-#         tempVar = IntVar()
-#         tempVar.set(data[i])
-#         self.VarList.append(tempVar)
-#     print('thread', self.VarList)
-#     timer = threading.Timer(1, update_data)
-#     timer.daemon = True
-#     timer.start()
-
-
-# timer = threading.Timer(1, update_data)
-# timer.daemon = True
-# timer.start()
+def update_data():
+    global VarList
+    VarList = get_data()
+    timer_update = threading.Timer(5, update_data)
+    timer_update.start()
+    
+def Curve():
+    CurveGUI()
 
 def do_job():
     print('I am here')
 
+timer_update = threading.Timer(5, update_data)
+timer_update.daemon = True
+thread_curve = threading.Thread(target=Curve, name='Plot')
+thread_curve.daemon = True
 
 class MainGUI:
     def __init__(self):
@@ -51,29 +39,14 @@ class MainGUI:
         self.Width = 390            # 窗口宽度
         self.Height = 360           # 窗口高度
 
-        self.VarList = []
-
         self.root = Tk()         
         self.root.geometry(str(self.Width) + 'x' + str(self.Height))
         self.root.title('分布式环境监测系统')
 
         self.AddMenu()
-        
-
-        data = np.ones(12)
-
-        while True:
-            data += 1
-            time.sleep(1)
-            self.root.update()
-            self.root.after(10)
-            for i in range(len(data)):
-                tempVar = IntVar()
-                tempVar.set(data[i])
-                self.VarList.append(tempVar)
-            self.AddLabel()
-            self.VarList = []
-        
+        timer_update.start()
+        timer_update.join()
+        self.AddLabel()
         
         self.root.mainloop()
     
@@ -109,19 +82,20 @@ class MainGUI:
         self.root['menu'] = self.menubar
 
     def AddLabel(self):
+        global VarList
         lbl1 = Label(self.root, text="节点1", font=("黑体", 20))
         lbl1.grid(column = 0, row = 0)
         LT1 = Label(self.root, text = '温度：', font=("黑体", 15))
         LT1.grid(column=0, row=1)
-        T1 = Label(self.root, text = str(self.VarList[0].get()) + ' ', font=("黑体", 15))
+        T1 = Label(self.root, text = str(VarList[0]) + ' ', font=("黑体", 15))
         T1.grid(column=1, row=1)
         LH1 = Label(self.root, text = '湿度：', font=("黑体", 15))
         LH1.grid(column=0, row=2)
-        H1 = Label(self.root, text = str(self.VarList[1].get()) + ' ', font=("黑体", 15))
+        H1 = Label(self.root, text = str(VarList[1]) + ' ', font=("黑体", 15))
         H1.grid(column=1, row=2)
         LL1 = Label(self.root, text = '光照：', font=("黑体", 15))
         LL1.grid(column=0, row=3)
-        L1 = Label(self.root, text = str(self.VarList[2].get()) + ' ', font=("黑体", 15))
+        L1 = Label(self.root, text = str(VarList[2]) + ' ', font=("黑体", 15))
         L1.grid(column=1, row=3)
         enter = Label(self.root, text="\n", font=("黑体", 10))
         enter.grid(column=0, row=4)
@@ -130,15 +104,15 @@ class MainGUI:
         lbl2.grid(column=2, row=0)
         LT2 = Label(self.root, text = '    温度：', font=("黑体", 15))
         LT2.grid(column=2, row=1)
-        T2 = Label(self.root, text = str(self.VarList[3].get()) + ' ', font=("黑体", 15))
+        T2 = Label(self.root, text = str(VarList[3]) + ' ', font=("黑体", 15))
         T2.grid(column=3, row=1)
         LH2 = Label(self.root, text = '    湿度：', font=("黑体", 15))
         LH2.grid(column=2, row=2)
-        H2 = Label(self.root, text = str(self.VarList[4].get()) + ' ', font=("黑体", 15))
+        H2 = Label(self.root, text = str(VarList[4]) + ' ', font=("黑体", 15))
         H2.grid(column=3, row=2)
         LL2 = Label(self.root, text = '    光照：', font=("黑体", 15))
         LL2.grid(column=2, row=3)
-        L2 = Label(self.root, text = str(self.VarList[5].get()) + ' ', font=("黑体", 15))
+        L2 = Label(self.root, text = str(VarList[5]) + ' ', font=("黑体", 15))
         L2.grid(column=3, row=3)
         enter = Label(self.root, text="\n", font=("黑体", 10))
         enter.grid(column=2, row=4)
@@ -147,15 +121,15 @@ class MainGUI:
         lbl3.grid(column = 0, row = 5)
         LT3 = Label(self.root, text = '温度：', font=("黑体", 15))
         LT3.grid(column=0, row=6)
-        T3 = Label(self.root, text = str(self.VarList[6].get()) + ' ', font=("黑体", 15))
+        T3 = Label(self.root, text = str(VarList[6]) + ' ', font=("黑体", 15))
         T3.grid(column=1, row=6)
         LH3 = Label(self.root, text = '湿度：', font=("黑体", 15))
         LH3.grid(column=0, row=7)
-        H3 = Label(self.root, text = str(self.VarList[7].get()) + ' ', font=("黑体", 15))
+        H3 = Label(self.root, text = str(VarList[7]) + ' ', font=("黑体", 15))
         H3.grid(column=1, row=7)
         LL3 = Label(self.root, text = '光照：', font=("黑体", 15))
         LL3.grid(column=0, row=8)
-        L3 = Label(self.root, text = str(self.VarList[8].get()) + ' ', font=("黑体", 15))
+        L3 = Label(self.root, text = str(VarList[8]) + ' ', font=("黑体", 15))
         L3.grid(column=1, row=8)
         # enter = Label(self.root, text="\n", font=("黑体", 25))
         # enter.grid(column=0, row=9)
@@ -164,18 +138,19 @@ class MainGUI:
         lbl4.grid(column=2, row=5)
         LT4 = Label(self.root, text = '    温度：', font=("黑体", 15))
         LT4.grid(column=2, row=6)
-        T4 = Label(self.root, text = str(self.VarList[9].get()) + ' ', font=("黑体", 15))
+        T4 = Label(self.root, text = str(VarList[9]) + ' ', font=("黑体", 15))
         T4.grid(column=3, row=6)
         LH4 = Label(self.root, text = '    湿度：', font=("黑体", 15))
         LH4.grid(column=2, row=7)
-        H4 = Label(self.root, text = str(self.VarList[10].get()) + ' ', font=("黑体", 15))
+        H4 = Label(self.root, text = str(VarList[10]) + ' ', font=("黑体", 15))
         H4.grid(column=3, row=7)
         LL4 = Label(self.root, text = '    光照：', font=("黑体", 15))
         LL4.grid(column=2, row=8)
-        L4 = Label(self.root, text = str(self.VarList[11].get()) + ' ', font=("黑体", 15))
+        L4 = Label(self.root, text = str(VarList[11]) + ' ', font=("黑体", 15))
         L4.grid(column=3, row=8)
         # enter = Label(self.root, text="\n", font=("黑体", 10))
         # enter.grid(column=2, row=9)
+        VarList = []
 
     def HelpDoc(self):
         self.DocGUI = Tk()
@@ -190,8 +165,11 @@ class MainGUI:
         messagebox.showinfo("关于", str)
 
     def DrawCurve(self):
-        self.curve = CurveGUI()
+        thread_curve.start()
 
-    
-if __name__ == '__main__':
+def CreateGUI():
     MainGUI()
+
+# thread_main = threading.Thread(target=CreateGUI, name='Main')
+if __name__ == '__main__':
+    CreateGUI()
